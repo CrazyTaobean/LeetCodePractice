260120dc62be2d2d1c9c24bf09e320b6b7aecd1b
zhudaobin <15247278965@163.com>
2022-03-05 17:01:55 +0800

    Init and add two problem i have solved

---
+++ cmake-build-debug/CMakeFiles/LeetCode2022.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeetCode2022.dir/main.cpp.obj"
+  "LeetCode2022.exe"
+  "LeetCode2022.exe.manifest"
+  "LeetCode2022.pdb"
+  "libLeetCode2022.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LeetCode2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
